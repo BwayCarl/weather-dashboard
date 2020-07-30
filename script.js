d63d39bffd48aac5df711a6517e3478ea47a5e42
Carlos <bwaycarl@gmail.com>
2020-07-30 15:21:47 -0400

    prepending buttons for searched cities.

--- script.js
+++ script.js
@@ -24,17 +24,17 @@ $("#find-city").on("click", function(event) {
 
 //Render buttons for searched cities
 
-let cities = ["New York City"];
+let cities = [];
 
 function renderCityButtons() {
-    $("#city-buttons-view").empty();
+    $("#city-button").empty();
 
     // Looping through the array of cities
     for (var i = 0; i < cities.length; i++) {
 
         // Then dynamically generating buttons for each city in the array.
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-        var newCity = $("<button>");
+        // This code $("<li>") is all jQuery needs to create the start and end tag. (<button></button>)
+        var newCity = $("<li>");
         // Adding a class
         newCity.addClass("city");
         // Adding a data-attribute with a value of the city at index i
@@ -42,21 +42,21 @@ function renderCityButtons() {
         // Providing the button's text with a value of the city at index i
         newCity.text(cities[i]);
         // Adding the button to the HTML
-        $("#buttons-view").append(newCity);
+        $("#city-button").prepend(newCity);
       }
     }
 
     // This function handles events where one button is clicked
-    $("#add-movie").on("click", function(event) {
+    $("#search-btn").on("click", function(event) {
       // event.preventDefault() prevents the form from trying to submit itself.
       // We're using a form so that the user can hit enter instead of clicking the button if they want
       event.preventDefault();
 
       // This line will grab the text from the input box
       var city = $("#city-input").val().trim();
-      // The movie from the textbox is then added to our array
+      // The city from the textbox is then added to our array
       cities.push(city);
 
-      // calling renderButtons which handles the processing of our movie array
-      renderButtons();
+      // calling renderButtons which handles the processing of our city array
+      renderCityButtons();
     });
