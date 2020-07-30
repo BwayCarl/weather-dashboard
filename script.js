972d9b35164f3182f45993f2ca746f69a5a61d16
Carlos <bwaycarl@gmail.com>
2020-07-30 17:17:26 -0400

    Update.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const today = moment().format("dddd, MMMM Do, YYYY"); // Current day
 
+
+
 $(document).ready(function() {
 });
 
 $("#currentDay").append(today) // Adds curent day to the page.
+
 
 
 
@@ -32,31 +35,30 @@ function renderCityButtons() {
     // Looping through the array of cities
     for (var i = 0; i < cities.length; i++) {
 
-        // Then dynamically generating buttons for each city in the array.
-        // This code $("<li>") is all jQuery needs to create the start and end tag. (<button></button>)
+        // Generate buttons for each city in the array.
         var newCity = $("<li>");
-        // Adding a class
         newCity.addClass("city");
-        // Adding a data-attribute with a value of the city at index i
         newCity.attr("data-name", cities[i]);
-        // Providing the button's text with a value of the city at index i
         newCity.text(cities[i]);
-        // Adding the button to the HTML
+        
         $("#city-button").prepend(newCity);
       }
     }
 
-    // This function handles events where one button is clicked
+    // Search button on click for new city search.
     $("#search-btn").on("click", function(event) {
-      // event.preventDefault() prevents the form from trying to submit itself.
-      // We're using a form so that the user can hit enter instead of clicking the button if they want
       event.preventDefault();
 
-      // This line will grab the text from the input box
       var city = $("#city-input").val().trim();
-      // The city from the textbox is then added to our array
       cities.push(city);
 
-      // calling renderButtons which handles the processing of our city array
       renderCityButtons();
     });
+
+    // Retrieve weather info from previously rendered buttons in the search history.
+    $("#city-button").on("click", function(event) {
+        event.preventDefault();
+  
+        
+      });
+  
