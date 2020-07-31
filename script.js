565db34ca1f9b42eb83fc5dc90b6bbf26eca1d5a
Carlos <bwaycarl@gmail.com>
2020-07-31 13:06:51 -0400

    Setting up variables, API, AJAX & JSON functions.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+//Variables
 const today = moment().format("dddd, MMMM Do, YYYY"); // Current day
 
+const cityName = document.getElementById("#city-input"); // City search
+const search =  document.getElementById("#search-btn"); // Search button
+
+const temperature = document.getElementById("#temp"); // Current Temperature
+const humidity = document.getElementById("hum"); // Current Humidity
+const windSpeed = document.getElementById("#wind"); // Current Wind Speed
+const uvIndex = document.getElementById("#uv"); // Current UV index
+
+const apiKey = "&APPID=5accc33209d1c0dd9925ae90d4b60f93"; // API Key
+
+
+
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
 
 
 $(document).ready(function() {
@@ -10,22 +25,22 @@ $("#currentDay").append(today) // Adds curent day to the page.
 
 
 
-$("#find-city").on("click", function(event) {
+$(search).on("click", function(event) {
     event.preventDefault();
 
     let city = $("#city-input").val();
-    let queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + city + "5accc33209d1c0dd9925ae90d4b60f93";
+    let queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        $("#city-view").text(JSON.stringify(response));
+        $("#city-input").text(JSON.stringify(response));
     });
-
+console.log(city)
 });
 
-//Render buttons for searched cities
+//Create buttons for searched cities
 
 let cities = [];
 
@@ -41,7 +56,7 @@ function renderCityButtons() {
         newCity.attr("data-name", cities[i]);
         newCity.text(cities[i]);
         
-        $("#city-button").prepend(newCity);
+        $("#city-button").prepend(newCity); //Most recent search lands on top of list.
       }
     }
 
